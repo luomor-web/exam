7b15a87f9d7a80d3a4018e88372a2f7b3217f170
大象科技 <7931965+elephant_soft@user.noreply.gitee.com>
2022-02-24 06:15:18 +0000

    删除文件 src

--- src/main/java/com/mwt/oes/service/StudentSearchService.java
+++
@@ -1,8 +0,0 @@
-package com.mwt.oes.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface StudentSearchService {
-    public List<Map<String, Object>> getSearchPapers(String keyword);
-}
